f45b43edaa5c4237307925dc6c118ac511e5a98f
SprLau <lau@buaa.edu.cn>
2022-01-26 23:19:00 +0800

    deploy

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,17 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---src-pages-404-tsx": preferDefault(require("/Users/springs/Documents/GitHub/SprLau.github.io/src/pages/404.tsx")),
+  "component---src-pages-about-tsx": preferDefault(require("/Users/springs/Documents/GitHub/SprLau.github.io/src/pages/about.tsx")),
+  "component---src-pages-blog-tsx": preferDefault(require("/Users/springs/Documents/GitHub/SprLau.github.io/src/pages/blog.tsx")),
+  "component---src-pages-index-tsx": preferDefault(require("/Users/springs/Documents/GitHub/SprLau.github.io/src/pages/index.tsx")),
+  "component---src-pages-projects-tsx": preferDefault(require("/Users/springs/Documents/GitHub/SprLau.github.io/src/pages/projects.tsx")),
+  "component---src-pages-tags-tsx": preferDefault(require("/Users/springs/Documents/GitHub/SprLau.github.io/src/pages/tags.tsx")),
+  "component---src-pages-works-tsx": preferDefault(require("/Users/springs/Documents/GitHub/SprLau.github.io/src/pages/works.tsx")),
+  "component---src-templates-post-tsx": preferDefault(require("/Users/springs/Documents/GitHub/SprLau.github.io/src/templates/post.tsx")),
+  "component---src-templates-tag-tsx": preferDefault(require("/Users/springs/Documents/GitHub/SprLau.github.io/src/templates/tag.tsx"))
+}
+
